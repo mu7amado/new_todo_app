a444190c636bd09592dec72a14fb4cf1e6e733e4
Moody <mu7amado.3li@gmail.com>
2023-07-15 22:04:30 +0300

    create web app

--- web.py
+++ web.py
@@ -4,6 +4,7 @@ import functions
 todos = functions.read_todo()
 
 
+st.set_page_config(layout="wide")
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
@@ -12,7 +13,11 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This is my todo app. ")
-st.write("This app is to increase your productivity. ")
+st.write("This app is to increase your <b>productivity</b>. ",
+         unsafe_allow_html=True)
+
+st.text_input(label="", placeholder="Add New To-Do...",
+              on_change=add_todo, key="new_todo")
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -22,7 +27,6 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.experimental_rerun()
 
-st.text_input(label="", placeholder="Add New To-Do...",
-              on_change=add_todo, key="new_todo")
+
 print("Hello")
 # st.session_state
